93191c67a9d3a6714fba1c6380e92ca14a5d1019
StockerAlexander <alex.ph.stocker@gmail.com>
2015-04-01 17:09:29 +0200

    Added NewView
    NewView is the coded layout.

---
+++ src/prototype/NewView.java
@@ -0,0 +1,12 @@
+package prototype;
+
+import javax.swing.JFrame;
+
+public class NewView extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
