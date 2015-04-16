698355f9947c8b613764aebb0abe5192b0e2c0e9
destalo94 <denis.stampfli@students.fhnw.ch>
2015-04-16 13:29:50 +0200

    Added Test for OppeltDim

---
+++ src/test/OppeltDimTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import static org.junit.Assert.*;
+import main.OppeltDim;
+import main.RegelStrecke;
+import main.Regler;
+
+import org.junit.Test;
+
+public class OppeltDimTest {
+
+	@Test
+	public void testCalc() {
+		RegelStrecke rs = new RegelStrecke(0.9999, 2.862, 20.24);
+		OppeltDim dim = new OppeltDim();
+		Regler regler = dim.calc(rs);
+		Regler regler_referenz = new Regler(8.487221888, 5.724, 1.20204);
+
+		assertEquals(regler.getKr(), regler_referenz.getKr(), 0.0001);
+		assertEquals(regler.getTn(), regler_referenz.getTn(), 0.0001);
+		assertEquals(regler.getTv(), regler_referenz.getTv(), 0.0001);
+	}
+}
