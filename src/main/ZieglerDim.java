bfc1f16a247e084163834b402df5c61713ca15ca
claudiusjoerg <claudius.joerg@bluewin.ch>
2015-04-17 13:39:58 +0200

    Class ZieglerDim Added
    Ziegler/Nichols Faustformel

---
+++ src/main/ZieglerDim.java
@@ -0,0 +1,18 @@
+package main;
+
+public final class ZieglerDim extends ReglerDim {
+
+	@Override
+	public Regler calc(RegelStrecke regelstrecke) {
+		double Ks = regelstrecke.getKs().getValue();
+		double Tu = regelstrecke.getTu().getValue();
+		double Tg = regelstrecke.getTg().getValue();
+		
+		return new Regler((0.9 / Ks) * (Tg / Tu), 2 * Tu, 0.5 * Tu);
+	}
+
+	@Override
+	public ReglerDim makeCopy() {
+		return this;
+	}
+}
