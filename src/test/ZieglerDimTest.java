b7bb5751c41c1656a7d9c9f8671527be0fac9a83
claudiusjoerg <claudius.joerg@bluewin.ch>
2015-04-20 18:32:03 +0200

    Fixed ZieglerDimTest

--- src/test/ZieglerDimTest.java
+++ src/test/ZieglerDimTest.java
@@ -11,10 +11,10 @@ public class ZieglerDimTest {
 
 	@Test
 	public void testCalc() {
-		RegelStrecke rs = new RegelStrecke(0.9999, 2.862, 20.24); //Testwerte für alle Faustformeln gleich?
+		RegelStrecke rs = new RegelStrecke(0.9999, 4.082, 13.24);
 		ZieglerDim dim = new ZieglerDim();
 		Regler regler = dim.calc(rs);
-		Regler regler_referenz = new Regler(6.3654, 5.724, 1.4310); //Werte Kr, Tn, Tv mit Matlab berechnet
+		Regler regler_referenz = new Regler(2.919449221, 8.164, 2.041);
 
 		assertEquals(regler.getKr(), regler_referenz.getKr(), 0.0001);
 		assertEquals(regler.getTn(), regler_referenz.getTn(), 0.0001);
