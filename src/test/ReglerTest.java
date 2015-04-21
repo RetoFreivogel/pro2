8979959c972b6b6eba2769a6180af98b2cc52697
Reto Freivogel <rfreivogel@windowslive.com>
2015-04-21 08:46:02 +0200

    Added tests for Regler

--- src/test/ReglerTest.java
+++ src/test/ReglerTest.java
@@ -12,8 +12,29 @@ public class ReglerTest {
 	public void copyConstructorMakesAnEqualRegler() {
 		Regler regler = new Regler(1.0, 0.5, 0.0);
 		Regler reglercopy = new Regler(regler);
-		assertFalse(regler == reglercopy);
-		assertTrue(regler.equals(reglercopy));
+		assertNotSame(regler, reglercopy);
+		assertEquals(regler, reglercopy);
+	}
+	
+	@Test
+	public void testEquals(){
+		Regler regler = new Regler(1.0, 0.5, 0.0);
+		
+		assertEquals(regler, regler);
+		assertNotEquals(regler, null);
+		assertNotEquals(regler, 1.0);
+		assertNotEquals(regler, new Regler(2.0, 0.5, 0.0));
+		assertNotEquals(regler, new Regler(1.0, 1.5, 0.0));
+		assertNotEquals(regler, new Regler(1.0, 0.5, 1.0));
+		assertEquals(regler, new Regler(1.0, 0.5, 0.0));
+	}
+	
+	@Test
+	public void testHash(){
+		Regler regler = new Regler(1.0, 0.5, 0.0);
+		Regler reglercopy = new Regler(regler);
+		
+		assertEquals(regler.hashCode(), reglercopy.hashCode());
 	}
 
 }
