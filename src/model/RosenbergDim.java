0906ef6b072375daacba49cbd0ecbab3c870b33e
claudiusjoerg <claudius.joerg@bluewin.ch>
2015-05-04 09:39:54 +0200

    Expanded Rosenberg and -test

--- src/model/RosenbergDim.java
+++ src/model/RosenbergDim.java
@@ -8,7 +8,16 @@ public final class RosenbergDim extends AbstractDim {
 		double Tu = regelstrecke.getTu();
 		double Tg = regelstrecke.getTg();
 		
-		return new Regler((1.2 / Ks) * (Tg / Tu), 2 * Tu, 0.44 * Tu);
+		switch (topo) {
+		case P:
+			return new Regler((1.0 / Ks) * (Tg / Tu));
+		case PI:
+			return new Regler((0.91/ Ks) * (Tg / Tu), 3.3 * Tu);
+		case PID:
+			return new Regler((1.2 / Ks) * (Tg / Tu), 2 * Tu, 0.44 * Tu);
+		default:
+			throw new IllegalArgumentException();
+		}
 	}
 
 }
--- src/test/RosenbergDimTest.java
+++ src/test/RosenbergDimTest.java
@@ -14,12 +14,25 @@ public class RosenbergDimTest {
 	public void testCalc() {
 		RegelStrecke rs = new RegelStrecke(0.9999, 2.862, 20.24);
 		RosenbergDim dim = new RosenbergDim();
-		Regler regler = dim.calc(rs, ReglerTopologie.PID);
-		Regler regler_referenz = new Regler(8.487221888, 5.724, 1.25928);
+
+		Regler regler = dim.calc(rs, ReglerTopologie.P);
+		Regler regler_referenz = new Regler(7.072684907);
+
+		assertEquals(regler.getKr(), regler_referenz.getKr(), 0.0001);
+
+		regler = dim.calc(rs, ReglerTopologie.PI);
+		regler_referenz = new Regler(6.436143265, 9.4446);
+
+		assertEquals(regler.getKr(), regler_referenz.getKr(), 0.0001);
+		assertEquals(regler.getTn(), regler_referenz.getTn(), 0.0001);
+
+		regler = dim.calc(rs, ReglerTopologie.PID);
+		regler_referenz = new Regler(8.487221888, 5.724, 1.25928);
 
 		assertEquals(regler.getKr(), regler_referenz.getKr(), 0.0001);
 		assertEquals(regler.getTn(), regler_referenz.getTn(), 0.0001);
 		assertEquals(regler.getTv(), regler_referenz.getTv(), 0.0001);
+
 	}
 
 }
