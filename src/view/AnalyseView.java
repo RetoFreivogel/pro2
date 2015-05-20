acd2e9a5b059bbde62645bd585a133192fcbe1e3
Reto Freivogel <rfreivogel@windowslive.com>
2015-05-20 14:03:58 +0200

    Removed Error Integrals

--- src/view/AnalyseView.java
+++ src/view/AnalyseView.java
@@ -21,10 +21,6 @@ public class AnalyseView extends JPanel{
 	private JFormattedTextField tf_Ymax;
 	private JFormattedTextField tf_Tan;
 	private JFormattedTextField tf_Taus;
-	private JFormattedTextField tf_E;
-	private JFormattedTextField tf_E2;
-	private JFormattedTextField tf_Et;
-	private JFormattedTextField tf_E2t;
 
 	public AnalyseView(RegelKreis regelkreis, Controller controller) {
 		super();
@@ -59,30 +55,6 @@ public class AnalyseView extends JPanel{
 		tf_Taus = new JFormattedTextField(format);
 		tf_Taus.setEditable(false);
 		add(tf_Taus);
-
-		JLabel lb_1 = new JLabel("S |e(t)| dt");
-		add(lb_1);
-		tf_E = new JFormattedTextField(format);
-		tf_E.setEditable(false);
-		add(tf_E);
-
-		JLabel lb_2 = new JLabel("S e(t)^2 dt");
-		add(lb_2);
-		tf_E2 = new JFormattedTextField(format);
-		tf_E2.setEditable(false);
-		add(tf_E2);
-
-		JLabel lb_3 = new JLabel("S |e(t)|*t dt");
-		add(lb_3);
-		tf_Et = new JFormattedTextField(format);
-		tf_Et.setEditable(false);
-		add(tf_Et);
-
-		JLabel lb_4 = new JLabel("S e(t)^2*t dt");
-		add(lb_4);
-		tf_E2t = new JFormattedTextField(format);
-		tf_E2t.setEditable(false);
-		add(tf_E2t);
 		
 		update(regelkreis);
 	}
@@ -93,10 +65,6 @@ public class AnalyseView extends JPanel{
 		tf_Taus.setValue(sa.getTaus());
 		tf_Tan.setValue(sa.getTan());
 		tf_Ymax.setValue(sa.getYmax());
-		tf_E.setValue(sa.getE());
-		tf_E2.setValue(sa.getEquad());
-		tf_Et.setValue(sa.getEtime());
-		tf_E2t.setValue(sa.getEquadtime());
 	}
 
 }
